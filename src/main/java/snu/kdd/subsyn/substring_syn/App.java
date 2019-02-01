35f4b75b4b0eace6ca500b161f411f67de100b15
ghsong <ghsong@kdd.snu.ac.kr>
2019-02-01 15:21:07 +0900

    Create maven project

---
+++ src/main/java/snu/kdd/subsyn/substring_syn/App.java
@@ -0,0 +1,13 @@
+package snu.kdd.subsyn.substring_syn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
