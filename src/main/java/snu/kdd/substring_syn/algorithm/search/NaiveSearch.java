08ff00bd43ad0e03d01c2c39fc8725918225dc2e
ghsong <ghsong@kdd.snu.ac.kr>
2019-05-29 12:48:51 +0900

    log NaiveSearch

--- src/main/java/snu/kdd/substring_syn/algorithm/search/NaiveSearch.java
+++ src/main/java/snu/kdd/substring_syn/algorithm/search/NaiveSearch.java
@@ -3,6 +3,7 @@ package snu.kdd.substring_syn.algorithm.search;
 import snu.kdd.substring_syn.data.IntPair;
 import snu.kdd.substring_syn.data.Record;
 import snu.kdd.substring_syn.data.Subrecord;
+import snu.kdd.substring_syn.utils.Util;
 import snu.kdd.substring_syn.utils.window.RecordSortedSlidingWindow;
 import vldb18.NaivePkduckValidator;
 
@@ -23,13 +24,19 @@ public class NaiveSearch extends AbstractSearch {
 	}
 	
 	protected void searchRecordFromText( Record qrec, Record rec ) {
-		NaivePkduckValidator validator = new NaivePkduckValidator();
+		log.debug("searchRecordFromText(%d, %d)", ()->qrec.getID(), ()->rec.getID());
 		for ( int w=1; w<=rec.size(); ++w ) {
 			RecordSortedSlidingWindow windowSlider = new RecordSortedSlidingWindow(rec, w, theta);
 			for ( int widx=0; windowSlider.hasNext(); ++widx ) {
 				Subrecord window = windowSlider.next();
 				double sim = validator.sim(qrec, window.toRecord());
+				log.debug("qrec=%d, rec=%d, w=%d, widx=%d, sim=%.3f", qrec.getID(), rec.getID(), w, widx, sim);
 				if ( sim >= theta ) {
+					log.debug("qrec: %s", ()->qrec);
+					log.debug("window: %s", ()->window.toString());
+					log.debug("qrec_prefix: %s", ()->Util.getPrefix(qrec, theta));
+					log.debug("window_prefix: %s", ()->Util.getPrefix(window.toRecord(), theta));
+					log.debug("rec: %s", ()->rec.toStringDetails());
 					rsltFromText.add(new IntPair(qrec.getID(), rec.getID()));
 					return;
 				}
