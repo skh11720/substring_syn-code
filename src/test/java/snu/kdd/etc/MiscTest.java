804b86a74c148376b3a12c33e06602eafa7e00aa
ghsong <ghsong@kdd.snu.ac.kr>
2019-06-18 14:23:14 +0900

    maxTransLenOfSuffix test

--- src/test/java/snu/kdd/etc/MiscTest.java
+++ src/test/java/snu/kdd/etc/MiscTest.java
@@ -62,23 +62,24 @@ public class MiscTest {
 	@Test
 	public void testMaxTransLenOfSuffix() throws IOException {
 		Dataset dataset = Util.getDatasetWithPreprocessing("SPROT_long", "1000");
+		long ts = System.nanoTime();
 		for ( Record rec : dataset.searchedList ) {
-			if ( rec.size() - rec.getMinTransLength() < 0 || rec.getMaxTransLength() - rec.size() < 2 ) continue;
-			System.out.println("min/0/maxTransLen: "+rec.getMinTransLength()+", "+rec.size()+", "+rec.getMaxTransLength());
-			System.out.println( rec.toStringDetails() );
+//			if ( rec.size() - rec.getMinTransLength() < 0 || rec.getMaxTransLength() - rec.size() < 2 ) continue;
+//			System.out.println("min/0/maxTransLen: "+rec.getMinTransLength()+", "+rec.size()+", "+rec.getMaxTransLength());
+//			System.out.println( rec.toStringDetails() );
 			int[] len = new int[rec.size()+1];
 			for ( int sidx=0; sidx<rec.size(); ++sidx ) {
 				getMaxTransLenOfSuffix(len, rec, sidx);
-				System.out.println( "sidx: "+sidx+"\t"+Arrays.toString(len) );
+//				System.out.println( "sidx: "+sidx+"\t"+Arrays.toString(len) );
 			}
-			System.in.read();
 		}
+		System.out.println("testMaxTransLenOfSuffix(): "+((System.nanoTime()-ts)/1e6)+" ms");
 	}
 
 	public static void getMaxTransLenOfSuffix( int[] len, Record rec, int sidx ) {
 		len[sidx] = 0;
 		for ( int i=sidx+1; i<=rec.size(); ++i ) {
-			len[i] = Math.max(i-sidx, len[i-1]+1);
+			len[i] = Math.max( i-sidx, len[i-1]+1 );
 			for ( Rule rule : rec.getSuffixApplicableRules(i-1) ) {
 				// TODO: optimize this for loop ... so that iterates (|lhs|, |rhs|) pairs s.t. |lhs|<|rhs|)
 				int l = rule.lhsSize();
