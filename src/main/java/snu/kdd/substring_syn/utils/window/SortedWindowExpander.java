543ddce604cc07692e6ac60a743845d46214058d
ghsong <ghsong@kdd.snu.ac.kr>
2019-07-02 15:06:34 +0900

    PrefixSearch 1.06
    Fix bug in query side length filter
    (not tested)

---
+++ src/main/java/snu/kdd/substring_syn/utils/window/SortedWindowExpander.java
@@ -0,0 +1,82 @@
+package snu.kdd.substring_syn.utils.window;
+
+import java.util.Iterator;
+
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
+import snu.kdd.substring_syn.data.Record;
+import snu.kdd.substring_syn.data.Subrecord;
+
+public class SortedWindowExpander implements Iterator<Subrecord> {
+
+	final IntSet tokenSet;
+	final IntSet prefix;
+	final IntArrayList list;
+	final Record rec;
+	final int widx;
+	final double theta;
+	int w = 0;
+	
+	public SortedWindowExpander( Record rec, int widx, double theta ) {
+		tokenSet = new IntOpenHashSet();
+		prefix = new IntOpenHashSet();
+		list = new IntArrayList();
+		this.rec = rec;
+		this.widx = widx;
+		this.theta = theta;
+	}
+	
+	private int getPrefixLen() {
+		return w - (int)(Math.ceil(w*theta)) + 1;
+	}
+
+	private void insert( int key ) {
+		int idx = binarySearch(key);
+		if ( idx < 0 ) idx = -idx-1;
+		list.add(idx, key);
+		tokenSet.add(key);
+	}
+
+	private int binarySearch( int key ) {
+		int l = 0; 
+		int r = list.size();
+		while ( l < r ) {
+			int m = (l+r)/2;
+			int mKey = list.getInt(m);
+			if ( key < mKey ) r = m;
+			else if ( key > mKey ) l = m+1;
+			else return m;
+		}
+		return -l-1;
+	}
+
+	public IntSet getPrefix() {
+		int prefixLen = getPrefixLen();
+//		System.out.println("w: "+w);
+//		System.out.println("prefixLen: "+prefixLen);
+//		System.out.println("list.size: "+list.size());
+		return new IntOpenHashSet( list.subList(0, prefixLen) );
+	}
+	
+	public int getSetSize() {
+		return tokenSet.size();
+	}
+	
+//	public int getSize() {
+//		return w;
+//	}
+	
+	@Override
+	public boolean hasNext() {
+		return widx+w < rec.size();
+	}
+
+	@Override
+	public Subrecord next() {
+		int key = rec.getToken(widx+w);
+		if ( !tokenSet.contains(key) ) insert(key);
+		++w;
+		return new Subrecord(rec, widx, widx+w);
+	}
+}
