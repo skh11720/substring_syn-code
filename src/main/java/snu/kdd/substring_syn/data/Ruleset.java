faea4a37b31503b54f5ba9cce3f40e29f569b886
ghsong <ghsong@kdd.snu.ac.kr>
2019-08-09 15:10:14 +0900

    update
    memory usage
    refactor
    prevent possible error

--- src/main/java/snu/kdd/substring_syn/data/Ruleset.java
+++ src/main/java/snu/kdd/substring_syn/data/Ruleset.java
@@ -9,7 +9,6 @@ import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 public class Ruleset {
 	final String path;
 	final ObjectArrayList<Rule> ruleList;
-	public ACAutomataR automata;
 
 	public Ruleset( String rulePath, Iterable<Integer> distinctTokens, TokenIndex tokenIndex ) throws IOException {
 		this.path = rulePath;
@@ -17,8 +16,6 @@ public class Ruleset {
 		
 		createSelfRules(distinctTokens);
 		loadRulesFromFile(tokenIndex);
-		
-		automata = new ACAutomataR(ruleList);
 	}
 	
 	private void createSelfRules( Iterable<Integer> distinctTokens ) {
