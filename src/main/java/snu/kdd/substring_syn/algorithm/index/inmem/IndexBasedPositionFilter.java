47c56181d55074c5672e33d06b033b99dded0aac
ghsong <ghsong@kdd.snu.ac.kr>
2019-09-17 11:31:45 +0900

    update log (comment out)

--- src/main/java/snu/kdd/substring_syn/algorithm/index/inmem/IndexBasedPositionFilter.java
+++ src/main/java/snu/kdd/substring_syn/algorithm/index/inmem/IndexBasedPositionFilter.java
@@ -245,8 +245,8 @@ public class IndexBasedPositionFilter extends AbstractIndexBasedFilter implement
 						continue;
 					}
 					++num;
-					Log.log.trace("findSegmentRanges: rec.id=%d, sidx=%d, eidx=%d, score=%.3f, theta=%.3f", ()->rec.getID(), ()->sidx, ()->eidx, ()->score, ()->theta);
 					double score = (double)num/(query.size() + transLen.getLB(sidx, eidx) - num) + 1e5; // add eps to resolve precision problem
+//					Log.log.trace("findSegmentRanges: rec.id=%d, sidx=%d, eidx=%d, score=%.3f, theta=%.3f, comp=%s", ()->rec.getID(), ()->sidx, ()->eidx, ()->score, ()->theta, ()->(score>=theta));
 					if ( score >= theta ) {
 						if ( rangeList.size() > 0 && rangeList.get(rangeList.size()-1).min == sidx ) rangeList.get(rangeList.size()-1).max = eidx;
 						else rangeList.add(new IntRange(sidx, eidx));
