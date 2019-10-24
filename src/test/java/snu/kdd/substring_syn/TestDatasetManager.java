60ae9eff2af2a5f771043740a93a6f1a4eb8e5c2
ghsong <ghsong@kdd.snu.ac.kr>
2019-10-24 17:55:16 +0900

    TestDatasetManager

---
+++ src/test/java/snu/kdd/substring_syn/TestDatasetManager.java
@@ -0,0 +1,106 @@
+package snu.kdd.substring_syn;
+
+import java.io.IOException;
+
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import snu.kdd.pkwise.TransWindowDataset;
+import snu.kdd.pkwise.WindowDataset;
+import snu.kdd.substring_syn.data.Dataset;
+import snu.kdd.substring_syn.utils.Util;
+
+public class TestDatasetManager {
+
+	private static TestDatasetManager manager = new TestDatasetManager();
+	private Object2ObjectMap<DatasetKey, Dataset> map = new Object2ObjectOpenHashMap<>();
+	
+	public static final Dataset getDataset( String datasetName, String size, String nr, String qlen ) {
+		DatasetKey key = new DatasetKey(0, datasetName, size, nr, qlen, "0");
+		return manager.getDataset(key);
+	}
+
+	public static final WindowDataset getWindowDataset( String datasetName, String size, String nr, String qlen ) {
+		DatasetKey key = new DatasetKey(1, datasetName, size, nr, qlen, "0");
+		return (WindowDataset) manager.getDataset(key);
+	}
+
+	public static final TransWindowDataset getTransWindowDataset( String datasetName, String size, String nr, String qlen, String theta ) {
+		DatasetKey key = new DatasetKey(2, datasetName, size, nr, qlen, theta);
+		return (TransWindowDataset) manager.getDataset(key);
+	}
+	
+	private final Dataset getDataset( DatasetKey key ) {
+		createDatasetIfNotExists(key);
+		return map.get(key);
+	}
+	
+	private final void createDatasetIfNotExists( DatasetKey key ) {
+		if ( !map.containsKey(key) ) {
+			Dataset dataset = null;
+			try {
+				switch (key.type) {
+				case 0: dataset = createPlainDataset(key); break;
+				case 1: dataset = createWindowDataset(key); break;
+				case 2: dataset = createTransWindowDataset(key); break;
+				}
+			} catch ( IOException e ) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			map.put(key, dataset);
+		}
+	}
+	
+	private final Dataset createPlainDataset( DatasetKey key ) throws IOException {
+		return Dataset.createInstanceByName(key.getName(), key.getSize(), key.getNr(), key.getQlen());
+	}
+	
+	private final WindowDataset createWindowDataset( DatasetKey key ) throws IOException {
+		return Dataset.createWindowInstanceByName(key.getName(), key.getSize(), key.getNr(), key.getQlen());
+	}
+	
+	private final TransWindowDataset createTransWindowDataset( DatasetKey key ) throws IOException {
+		return Dataset.createTransWindowInstanceByName(key.getName(), key.getSize(), key.getNr(), key.getQlen(), key.getTheta());
+	}
+	
+	
+	
+	static final class DatasetKey {
+		
+		final int type;
+		final String[] fields;
+		
+		public DatasetKey( int type, String datasetName, String size, String nr, String qlen, String theta ) {
+			this.type = type;
+			fields = new String[5];
+			fields[0] = datasetName;
+			fields[1] = size;
+			fields[2] = nr;
+			fields[3] = qlen;
+			fields[4] = theta;
+		}
+		
+		public final String getName() { return fields[0]; }
+		public final String getSize() { return fields[1]; }
+		public final String getNr() { return fields[2]; }
+		public final String getQlen() { return fields[3]; }
+		public final String getTheta() { return fields[4]; }
+		
+		@Override
+		public final int hashCode() {
+			int hash = 0;
+			for ( int i=0; i<fields.length; ++i ) hash = (hash << 5) + fields[i].hashCode() % Util.bigprime;
+			return type*100 + hash%100;
+		}
+		
+		@Override
+		public final boolean equals(Object obj) {
+			if ( obj == null ) return false;
+			DatasetKey o = (DatasetKey) obj;
+			for ( int i=0; i<fields.length; ++i ) {
+				if ( !fields[i].equals(o.fields[i]) ) return false;
+			}
+			return true;
+		}
+	}
+}
