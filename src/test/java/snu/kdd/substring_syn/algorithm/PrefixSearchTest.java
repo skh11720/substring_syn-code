db582b770f0eb0f4b1846293cbe7b202bae5934d
ghsong (GHSONG-KDD-PC) <ghsong@kdd.snu.ac.kr>
2020-02-14 19:33:13 +0900

    testVaryRecordPoolSize

--- src/test/java/snu/kdd/substring_syn/algorithm/PrefixSearchTest.java
+++ src/test/java/snu/kdd/substring_syn/algorithm/PrefixSearchTest.java
@@ -23,6 +23,7 @@ import snu.kdd.substring_syn.algorithm.search.PrefixSearch;
 import snu.kdd.substring_syn.data.Dataset;
 import snu.kdd.substring_syn.data.DatasetFactory;
 import snu.kdd.substring_syn.data.DatasetParam;
+import snu.kdd.substring_syn.data.RecordPool;
 import snu.kdd.substring_syn.utils.InputArgument;
 import snu.kdd.substring_syn.utils.Log;
 import snu.kdd.substring_syn.utils.Stat;
@@ -40,7 +41,7 @@ public class PrefixSearchTest {
 		DatasetParam param = new DatasetParam("AMAZON", "300000", "107836", "5", "1.0");
 		double theta = 0.6;
 		Dataset dataset = DatasetFactory.createInstanceByName(param);
-		AbstractSearch alg = new PositionPrefixSearch(theta, true, true, IndexChoice.CountPosition);
+		AbstractSearch alg = new PositionPrefixSearch(theta, false, false, IndexChoice.CountPosition);
 		alg.run(dataset);
 	}
 	
@@ -200,5 +201,55 @@ public class PrefixSearchTest {
 					+"\t"+alg1.getStatContainer().getStat(Stat.Num_QS_Result)
 					+"\t"+alg1.getStatContainer().getStat(Stat.Num_TS_Result));
 		}
+	}
+	
+	@Test
+	public void testVaryRecordPoolSize() throws IOException {
+		/*
+		AMAZON_n300000_r107836_q5_l1.0, theta=0.6
+		RecordPool.size     T_Total        T_QS        T_TS    T_QS_Rec    T_TS_Rec    N_QS_F    N_TS_F     N_Res  N_QS_Res  N_TS_Res
+			 	0.0e+00   81046.475   33125.389   38552.535   25966.848   19879.682   1835313    887680     57452     39434     55858
+				1.0e+03   81596.689   33555.919   39254.822   26783.134   20284.497   1835313    887680     57452     39434     55858
+				1.0e+04   81527.797   33960.685   37508.089   27058.285   20256.111   1835301    887665     57452     39434     55858
+				1.0e+05   83080.164   34524.922   38113.782   27633.903   20539.307   1834312    886424     57452     39434     55858
+				1.0e+06   89892.782   39711.307   40103.000   32825.453   19554.414   1679517    773187     57452     39434     55858
+				1.0e+07   47089.132   14832.757   23685.047    3983.474    5432.434    235646    219655     57452     39434     55858
+				1.0e+08   53995.918   11677.661   28738.215    4183.679    5969.517    235646    219655     57452     39434     55858
+
+		AMAZON_n100000_r107836_q5_l1.0, theta=0.6
+		RecordPool.size     T_Total        T_QS        T_TS    T_QS_Rec    T_TS_Rec    N_QS_F    N_TS_F     N_Res  N_QS_Res  N_TS_Res
+				0.0e+00   26273.659    9913.431   12318.754    7600.578    6214.313    610108    295078     22492     13255     22364
+				1.0e+03   24737.995    9606.849   11932.784    7572.674    6222.871    610108    295077     22492     13255     22364
+				1.0e+04   24804.512    9555.723   11969.028    7572.490    6222.015    610074    295045     22492     13255     22364
+				1.0e+05   24732.035    9723.573   11896.971    7739.803    6010.949    604249    290393     22492     13255     22364
+				1.0e+06   20303.906    5913.986    9815.224    3708.202    3584.801    265364    147716     22492     13255     22364
+				1.0e+07   16423.526    3030.785   10372.152    1147.806    1741.924     78648     73136     22492     13255     22364
+				1.0e+08   17128.892    5822.520    8263.366    1237.910    1711.962     78648     73136     22492     13255     22364
+
+		 */
+		DatasetParam param = new DatasetParam("AMAZON", "300000", "107836", "5", "1.0");
+		double theta = 0.6;
+		String outputTmpl = "%16.1e%12.3f%12.3f%12.3f%12.3f%12.3f%10d%10d%10d%10d%10d\n";
+		StringBuilder strbld = new StringBuilder(String.format("%16s%12s%12s%12s%12s%12s%10s%10s%10s%10s%10s\n", "RecordPool.size", "T_Total", "T_QS", "T_TS", "T_QS_Rec", "T_TS_Rec", "N_QS_F", "N_TS_F", "N_Res", "N_QS_Res", "N_TS_Res"));
+		for ( double size : new double[] {0, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8} ) {
+			RecordPool.BUFFER_SIZE = (int)size;
+			Dataset dataset = DatasetFactory.createInstanceByName(param);
+			AbstractSearch alg = new PositionPrefixSearch(theta, false, false, IndexChoice.CountPosition);
+			alg.run(dataset);
+			strbld.append(String.format(outputTmpl, 
+					size, 
+					Double.parseDouble(alg.getStat(Stat.Time_Total)), 
+					Double.parseDouble(alg.getStat(Stat.Time_QS_Total)), 
+					Double.parseDouble(alg.getStat(Stat.Time_TS_Total)), 
+					Double.parseDouble(alg.getStat("Time_QS_IndexFilter.getRecord")), 
+					Double.parseDouble(alg.getStat("Time_TS_IndexFilter.getRecord")),
+					Integer.parseInt(alg.getStat("Num_QS_RecordFault")), 
+					Integer.parseInt(alg.getStat("Num_TS_RecordFault")), 
+					Integer.parseInt(alg.getStat(Stat.Num_Result)), 
+					Integer.parseInt(alg.getStat(Stat.Num_QS_Result)), 
+					Integer.parseInt(alg.getStat(Stat.Num_TS_Result))
+			));
+		}
+		System.out.println(strbld.toString());
 	}
 }
