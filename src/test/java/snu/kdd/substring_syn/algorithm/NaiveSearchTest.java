32007c46ca7013182f4aaad289bb6cedb5671fcd
ghsong (GHSONG-KDD-PC) <ghsong@kdd.snu.ac.kr>
2020-02-21 21:26:30 +0900

    refactor

--- src/test/java/snu/kdd/substring_syn/algorithm/NaiveSearchTest.java
+++ src/test/java/snu/kdd/substring_syn/algorithm/NaiveSearchTest.java
@@ -14,6 +14,9 @@ import snu.kdd.substring_syn.algorithm.search.ExactNaiveSearch;
 import snu.kdd.substring_syn.data.Dataset;
 import snu.kdd.substring_syn.data.DatasetFactory;
 import snu.kdd.substring_syn.data.DatasetParam;
+import snu.kdd.substring_syn.utils.Log;
+import snu.kdd.substring_syn.utils.Stat;
+import snu.kdd.substring_syn.utils.StatContainer;
 
 @RunWith(Parameterized.class)
 public class NaiveSearchTest {
@@ -33,7 +36,8 @@ public class NaiveSearchTest {
 	@Parameters
 	public static Collection<Param> provideParams() {
 		ObjectList<Param> paramList = new ObjectArrayList<>();
-		double[] thetaList = {1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1};
+//		double[] thetaList = {1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1};
+		double[] thetaList = {1.0, 0.8, 0.6, 0.4, 0.2};
 		String[] sizeList = {"100"};
 		for ( double theta : thetaList ) {
 			for ( String size : sizeList ) {
@@ -49,6 +53,14 @@ public class NaiveSearchTest {
 
 	@Test
 	public void test() throws IOException {
+		/*
+		 1.0	1	1	1
+		 0.8	1	1	1
+		 0.6	6	6	6
+		 0.4	564	564	564
+		 0.2	5982	5981	5982
+		 */
+		Log.disable();
 		Dataset dataset = DatasetFactory.createInstanceByName(param.datasetParam);
 		
 		ExactNaiveSearch naiveSearch = new ExactNaiveSearch(param.theta);
@@ -56,6 +68,8 @@ public class NaiveSearchTest {
 		long ts = System.nanoTime();
 		naiveSearch.run(dataset);
 		long t = System.nanoTime() - ts;
-		System.out.println(t/1e6);
+		StatContainer stat = naiveSearch.getStatContainer();
+
+		System.out.printf("%4.1f\t%s\t%s\t%s\n", param.theta, stat.getStat(Stat.Num_Result), stat.getStat(Stat.Num_QS_Result), stat.getStat(Stat.Num_TS_Result));
 	}
 }
