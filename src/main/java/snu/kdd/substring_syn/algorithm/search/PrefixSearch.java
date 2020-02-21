d4c64745dc3ba88b697b5a7ce2e524d5e92dbb8c
ghsong (GHSONG-KDD-PC) <ghsong@kdd.snu.ac.kr>
2020-02-22 02:33:23 +0900

    PrefixSearch rollback 6.30.03 -> 6.30

--- src/main/java/snu/kdd/substring_syn/algorithm/search/PrefixSearch.java
+++ src/main/java/snu/kdd/substring_syn/algorithm/search/PrefixSearch.java
@@ -460,8 +460,7 @@ public class PrefixSearch extends AbstractIndexBasedSearch {
 		 * 6.28: fix bug in modified count filter, speedup preprocessing
 		 * 6.29: use RecordPool
 		 * 6.30: Reduce pool size=1e4
-		 * 6.30.03: Reduce pool size=1e7
 		 */
-		return "6.30.03";
+		return "6.30";
 	}
 }
