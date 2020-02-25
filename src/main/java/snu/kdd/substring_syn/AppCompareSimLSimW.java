83f628f5ac014860ecf7c6df859f65fef537bb17
ghsong (GHSONG-KDD-PC) <ghsong@kdd.snu.ac.kr>
2020-02-25 22:23:21 +0900

    fix AppCompareSimLSimW

--- src/main/java/snu/kdd/substring_syn/AppCompareSimLSimW.java
+++ src/main/java/snu/kdd/substring_syn/AppCompareSimLSimW.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -20,16 +21,23 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FileUtils;
 
+import snu.kdd.substring_syn.algorithm.search.AbstractIndexBasedSearch.IndexChoice;
+import snu.kdd.substring_syn.algorithm.search.variants.PerQueryPositionPrefixSearch;
 import snu.kdd.substring_syn.algorithm.validator.NaiveValidator;
 import snu.kdd.substring_syn.algorithm.validator.NaiveWindowBasedValidator;
 import snu.kdd.substring_syn.data.Dataset;
 import snu.kdd.substring_syn.data.DatasetFactory;
 import snu.kdd.substring_syn.data.DatasetParam;
+import snu.kdd.substring_syn.data.IntPair;
 import snu.kdd.substring_syn.data.record.Record;
 import snu.kdd.substring_syn.utils.Log;
 
 public class AppCompareSimLSimW {
 
+	static final double EPS = 1e-5;
+	static final int TIMEOUT = 5;
+	static ExecutorService executor = Executors.newSingleThreadExecutor();
+	static PrintWriter pw = null;
 	static NaiveValidator val0 = new NaiveValidator(0, null);
 	static NaiveWindowBasedValidator val1 = new NaiveWindowBasedValidator(0, null);
 	
@@ -59,37 +67,60 @@ public class AppCompareSimLSimW {
 		double theta = Double.parseDouble(cmd.getOptionValue("theta"));
 
 		String outputName = String.format("output/AppCompareSimLSimW_%s_%s_%s_%s_%s_%.1f.txt", dataName, size, nq, qlen, nr, theta);
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(outputName, false)));
+		pw = new PrintWriter(new BufferedWriter(new FileWriter(outputName, false)));
 		DatasetParam param = new DatasetParam(dataName, size, nr, qlen, "1.0");
 		Dataset dataset = DatasetFactory.createInstanceByName(param);
+		if (theta < EPS ) runNaive(dataset, nq);
+		else runAlg(dataset, nq, theta);
+		pw.close();
+    }
 
-		ExecutorService executor = Executors.newSingleThreadExecutor();
+    public static void runNaive(Dataset dataset, String nq) throws InterruptedException, ExecutionException {
 		for ( Record rec : dataset.getIndexedList() ) {
 			Log.log.info("rec.id=%d", rec.getID());
 			rec.preprocessAll();
 			for ( Record query : dataset.getSearchedList() ) {
-				double sim0 = -1, sim1 = -1;
-				Future<Double> future = null;
-				future = executor.submit(new Task0(query, rec));
-				try { sim0 = future.get(5, TimeUnit.SECONDS); }
-				catch (TimeoutException e) { future.cancel(true); }
-				if ( sim0 < 0 ) continue;
-				if ( sim0 < theta-1e-5 ) continue;
-				future = executor.submit(new Task1(query, rec));
-				try { sim1 = future.get(5, TimeUnit.SECONDS); }
-				catch (TimeoutException e) { future.cancel(true); }
-				if ( sim1 < 0 ) continue;
-
-				if( Math.abs(sim0-sim1) < 1e-5 ) pw.println("E");
-				else pw.printf("N\t%d %d %.6f %.6f\n", query.getID(), rec.getID(), sim0, sim1);
-//				Log.log.info("rec.id=%d query.id=%d", rec.getID(), query.getID());
+				verifyPair(query, rec);
 				if ( query.getID()-1 >= Integer.parseInt(nq) ) break;
 			}
 			pw.flush();
 		}
-		pw.close();
     }
     
+    public static void runAlg(Dataset dataset, String nq, double theta) throws InterruptedException, ExecutionException {
+		PerQueryPositionPrefixSearch alg = new PerQueryPositionPrefixSearch(dataset, theta, true, false, IndexChoice.CountPosition);
+		for ( Record query : dataset.getSearchedList() ) {
+			Set<IntPair> rslt = alg.searchTextSideGivenQuery(query);
+			for ( IntPair pair : rslt ) {
+				int rid = pair.i2;
+				Record rec = dataset.getRecord(rid);
+				rec.preprocessAll();
+				verifyPair(query, rec);
+				if ( query.getID()-1 >= Integer.parseInt(nq) ) break;
+			}
+			if ((query.getID()+1)%10000 == 0) {
+				Log.log.info("query.id=%d", query.getID());
+				pw.flush();
+			}
+		}
+		pw.flush();
+    }
+    
+    private static void verifyPair(Record query, Record rec) throws InterruptedException, ExecutionException {
+		double sim0 = -1, sim1 = -1;
+		Future<Double> future = null;
+		future = executor.submit(new Task0(query, rec));
+		try { sim0 = future.get(TIMEOUT, TimeUnit.SECONDS); }
+		catch (TimeoutException e) { future.cancel(true); }
+		if ( sim0 < EPS ) return;
+		future = executor.submit(new Task1(query, rec));
+		try { sim1 = future.get(TIMEOUT, TimeUnit.SECONDS); }
+		catch (TimeoutException e) { future.cancel(true); }
+		if ( sim1 < EPS ) return;
+
+		if( Math.abs(sim0-sim1) < EPS ) pw.println("E");
+		else pw.printf("N\t%d %d %.6f %.6f\n", query.getID(), rec.getID(), sim0, sim1);
+    }
     
     static class Task0 implements Callable<Double> {
     	
